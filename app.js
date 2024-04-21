23272ccf36dbc7a9dd8c1d769dea5b263a1dc581
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-04-21 19:30:13 -0400

    footer

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+const projects = [
+  {
+    img: "./assets/projects-demos/portfolio-v1.png",
+    title: "Personal Website v1",
+    tags: ["Html", "Css", "Javascript"],
+    website: "",
+  },
+];
+
 const navBtn = document.querySelector(".nav-btn");
 const menu = document.querySelector(".links");
 const navBar = document.querySelector(".navbar");
 const techImgs = document.querySelectorAll(".tech-img");
 const hero = document.querySelector(".header-img");
+const projectsContainer = document.querySelector(".projects-container");
+const contactBtn = document.querySelector(".contact-btn");
+const btnLink = document.querySelector(".btn-link");
+const year = document.querySelector("#year");
 
 navBtn.addEventListener("click", function () {
   if (menu.classList.contains("hide-navbar")) {
@@ -22,3 +35,39 @@ document.addEventListener("scroll", function () {
     navBar.classList.remove("sticking");
   }
 });
+
+window.addEventListener("DOMContentLoaded", function () {
+  displayProjects(projects);
+});
+
+function displayProjects(projectItem) {
+  let projectsHTML = projectItem.map(function (project) {
+    return `<article class="project">
+          <a href="${project.website}"><div class="main-project-container">
+            <img class="img" src="${project.img}" alt="">
+            <i class="fas fa-search-plus"></i>
+            <p class="project-title">${project.title}</p>
+          </div></a>
+          <div class="tags">
+            <button class="tag-btn">${project.tags[0]}</button>
+            <button class="tag-btn">${project.tags[1]}</button>
+            <button class="tag-btn">${project.tags[2]}</button>
+          </div>
+        </article>`;
+  });
+  projectsHTML = projectsHTML.join("");
+
+  projectsContainer.innerHTML = projectsHTML;
+}
+
+// Contact Button
+contactBtn.addEventListener("click", function () {
+  btnLink.href =
+    "mailto:bagoudoumoubarack@icloud.com?subject={Company%20Name}%20Job%20Interview&body=message%20goes%20here";
+});
+
+// Return current year
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
+year.textContent = currentYear;
